3a699c54cae26eca9945a4493a0baaf9fc83477a
Imanol-Ortega <imanicoortemohr018@gmail.com>
2024-09-16 18:22:17 -0400

    agg with

--- controllers/donado.controller.js
+++ controllers/donado.controller.js
@@ -24,7 +24,23 @@ export const guardarDonado = async(req,res)=>{
 
 export const obtenerDonados = async(req,res)=>{
     try {
-        
+        const query = `WITH DatosAgrupados AS (
+  SELECT
+  i.institucionid,
+    i.institucionnombre,
+    i.instituciondireccion,
+    STRING_AGG(i2.inventarionombre || ' (' || d.donadocantidad || ')', ', ') AS inventario_detallado,
+    SUM(d.donadocantidad) AS total_donado
+  FROM
+    Donado d
+  INNER JOIN Institucion i ON d.institucionid = i.institucionid
+  INNER JOIN Inventario i2 ON d.inventarioid = i2.inventarioid
+  GROUP BY
+    i.institucionnombre, i.instituciondireccion, i.institucionid
+)
+SELECT * FROM DatosAgrupados;` 
+        const result = await pool.query(query)
+        res.status(200).json(result.rows)
     } catch (error) {
         return res.status(500).json({message:error.message});
     }
